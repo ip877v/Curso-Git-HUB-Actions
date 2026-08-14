bd49781860fff1f199736e4763c3dbc55b1c7f91
ip877v <ip877v@mx.att.com>
2026-08-14 10:44:34 -0600

    Update Holamundo.py

--- Holamundo.py
+++ Holamundo.py
@@ -1,6 +1,10 @@
 import os
-def main ():
-  nombre = os.getenv("USERNAME")
-  print(f"¡Hola, {nombre} desde GitHuB"!
-if _name_ == "_main_":
-  main()
+
+
+def main():
+    nombre = os.getenv("USERNAME")
+    print(f"¡Hola, {nombre} desde GitHub!")
+
+
+if __name__ == "__main__":
+    main()
